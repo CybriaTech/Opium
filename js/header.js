b964388f0bfa05d5537240f8eb8755130dd79573
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-11-23 02:23:39 +0000

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,16 @@
+document.write(`
+  <header>
+    <a class="title" href="/">
+      <img src="images/logo/opium-clear.png" width="128" height="128">
+      <h2>Opium</h2>
+    </a>
+    <nav>
+      <a href="games.html">Games</a>
+      <a href="proxy.html">Proxy</a> 
+      <a href="bookmarklets.html">Bookmarklets</a> 
+      <a href="soundboard.html">Soundboard</a> 
+      <a href="about.html">About</a> 
+      <a href="settings.html"><i class="fas fa-cog"></i></a>
+    </nav>
+  </header>
+`);

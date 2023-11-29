9b324bfb3d9a20e3de263408b948abf052d3122d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-11-29 21:53:48 +0000

    Delete js/footer.js

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-document.write(`
-  <footer>
-  <p class="copyright">&copy CybriaTech 2023</p>
-  <nav>
-      <a href="https://github.com/CybriaTech" target="_blank"><i class="fab fa-github"></i></a>
-      <a href="https://bit.ly/cybriatech" target="_blank"><i class="fab fa-discord" style="color: #7289DA;"></i></a>
-      </nav>
-  </footer>
-`);

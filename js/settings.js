1d8d72bea0a48af3e95a244f61189514cbf05bb0
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-11-29 19:24:36 +0000

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,4 @@
-function schoology() {
+function openPopup(title, iconUrl) {
     let inFrame;
     
     try {
@@ -14,16 +14,15 @@ function schoology() {
         } else {
             const doc = popup.document;
 
-            const iframe = doc.createElement("iframe");
-            const style = iframe.style;
-
-            doc.title = "Home | Schoology";
+            doc.title = title;
 
             const link = doc.createElement("link");
             link.rel = "icon";
-            link.href = "/images/settings/schoology.ico";
+            link.href = iconUrl;
             doc.head.appendChild(link);
 
+            const iframe = doc.createElement("iframe");
+            const style = iframe.style;
             iframe.src = location.href;
             style.position = "fixed";
             style.top = style.bottom = style.left = style.right = 0;
@@ -37,41 +36,10 @@ function schoology() {
     }
 }
 
+function schoology() {
+    openPopup("Home | Schoology", "/images/settings/schoology.ico");
+}
+
 function ab() {
-    let inFrame;
-    
-    try {
-        inFrame = window !== top;
-    } catch (e) {
-        inFrame = true;
-    }
-
-    if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-        const popup = window.open("about:blank", "_blank");
-
-        if (!popup || popup.closed) {
-        } else {
-            const doc = popup.document;
-
-            const iframe = doc.createElement("iframe");
-            const style = iframe.style;
-
-            doc.title = "about:blank";
-
-            const link = doc.createElement("link");
-            link.rel = "icon";
-            link.href = "/images/settings/about-blank.png";
-            doc.head.appendChild(link);
-
-            iframe.src = location.href;
-            style.position = "fixed";
-            style.top = style.bottom = style.left = style.right = 0;
-            style.border = style.outline = "none";
-            style.width = style.height = "100%";
-
-            doc.body.appendChild(iframe);
-
-            location.replace("about:blank");
-        }
-    }
+    openPopup("about:blank", "/images/settings/about-blank.png");
 }

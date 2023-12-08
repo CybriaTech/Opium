56e52d1db59bc2c319664300033f691062ba53a1
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-12-08 07:35:15 +0000

    Cybriagames for some reason

--- js/header.js
+++ js/header.js
@@ -9,6 +9,8 @@ document.write(`
       <a href="/proxy.html">Proxy</a> 
       <a href="/bookmarklets.html">Bookmarklets</a> 
       <a href="/emulators">Emulators</a> 
+      <a href="https://cybriagames.pages.dev" style="text-decoration: none;">
+      <img src="/images/logo/cybriagames.png" alt="Cybria Games" style="width: 32px; height: 32px; vertical-align: middle;">Cybria Games</a>
       <a href="https://github.com/CybriaTech/Opium"><i class="fab fa-github"></i></a>
       <a href="/settings.html"><i class="fas fa-cog"></i></a>
     </nav>

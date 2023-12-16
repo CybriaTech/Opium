18ce73441e183d7e39410d9cedebad7bbbe76730
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-12-15 19:55:14 -0600

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,59 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var isWidgetbotActive = localStorage.getItem("widgetbotActive");
-    var isWidgetbotOn = isWidgetbotActive === null ? true : JSON.parse(isWidgetbotActive);
-    document.querySelector(".slider-checkbox").checked = isWidgetbotOn;
-    toggleWidgetbot(isWidgetbotOn);
-    document.querySelector(".slider-checkbox").addEventListener("change", function () {
-        isWidgetbotOn = this.checked;
-        toggleWidgetbot(isWidgetbotOn);
-        localStorage.setItem("widgetbotActive", JSON.stringify(isWidgetbotOn));
-    });
-});
-
-function toggleWidgetbot(isActive) {
-    var widgetbotContainer = document.getElementById("widgetbot");
-
-    if (isActive) {
-        widgetbotContainer.style.display = "block";
-        appendWidgetbotScript();
-    } else {
-        widgetbotContainer.style.display = "none";
-        removeWidgetbotScript();
-    }
-}
-
-function appendWidgetbotScript() {
-    var existingScript = document.getElementById("widgetbot-script");
-
-    if (!existingScript) {
-        var script = document.createElement("script");
-        script.id = "widgetbot-script";
-        script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
-        script.async = true;
-        script.defer = true;
-
-        document.getElementById("widgetbot").appendChild(script);
-
-        script.innerHTML = `
-            new Crate({
-                server: '1154632402942050394', // CybriaTech
-                channel: '1154632891310030878' // #guest-general
-            });
-        `;
-    }
-}
-
-function removeWidgetbotScript() {
-    var existingScript = document.getElementById("widgetbot-script");
-
-    if (existingScript) {
-        existingScript.remove();
-    }
-}
-
 function openPopup(title, iconUrl) {
     let inFrame;
-
+    
     try {
         inFrame = window !== top;
     } catch (e) {
@@ -74,17 +21,15 @@ function openPopup(title, iconUrl) {
             link.href = iconUrl;
             doc.head.appendChild(link);
 
-            if (isWidgetbotOn) {
-                const iframe = doc.createElement("iframe");
-                const style = iframe.style;
-                iframe.src = location.href;
-                style.position = "fixed";
-                style.top = style.bottom = style.left = style.right = 0;
-                style.border = style.outline = "none";
-                style.width = style.height = "100%";
+            const iframe = doc.createElement("iframe");
+            const style = iframe.style;
+            iframe.src = location.href;
+            style.position = "fixed";
+            style.top = style.bottom = style.left = style.right = 0;
+            style.border = style.outline = "none";
+            style.width = style.height = "100%";
 
-                doc.body.appendChild(iframe);
-            }
+            doc.body.appendChild(iframe);
 
             location.replace("about:blank");
         }

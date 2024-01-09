0f59a367b0b341049d6494c82c0b910e30056ba9
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2024-01-09 00:50:29 -0600

    lunaris is egg

--- js/header.js
+++ js/header.js
@@ -15,3 +15,29 @@ document.write(`
     </nav>
   </header>
 `);
+
+// lunaris flashback LOL (its an easter egg)
+document.addEventListener('lunaris', function() {
+  const h2Element = document.querySelector('header .title h2');
+  if (h2Element) {
+    h2Element.textContent = 'Lunaris';
+  }
+
+  const imgElement = document.querySelector('header .title img');
+  if (imgElement) {
+    imgElement.src = '/images/logo/lunaris.png';
+  }
+
+  const headerElement = document.querySelector('header');
+  if (headerElement) {
+    headerElement.style.backgroundColor = '#2E1FB7';
+  }
+  
+  document.title = 'Game Vault - Lunaris';
+
+  const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  favicon.type = 'image/x-icon';
+  favicon.rel = 'icon';
+  favicon.href = '/images/logo/lunaris.png';
+  document.head.appendChild(favicon);
+});

943cfed49aa0edd6ae245212570e2856eb609e72
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-01-11 23:31:40 +0000

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,15 +1,3 @@
-document.write(`
-               <div id="alert" class="alert">
-  <div class="contens">
-  <p style="font-weight: bold; font-size: 1.4em;">OPIUM UPDATE
-  </p>
-    <p>Currently, X-88 and Scott Ferren are updating Opium from Version 0.5 (Current) To Version 1.0. With newer feautres, games and content</p>
-    <button id="closing" class="closing">Oh Ok</button>
-  </div>
-</div>
-<div id="overlay" class="overlay"></div>
-  `);
-
 const alert = document.getElementById('alert');
 const overlay = document.getElementById('overlay');
 

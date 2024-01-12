0105a04390b0e1b82df5c51583214f5dd02a575f
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-01-12 16:49:19 -0600

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,85 +1,32 @@
-function save() {
-  var mainSave = {};
-
-  var localStorageDontSave = ["supportalert"];
-
-  localStorageSave = Object.entries(localStorage);
-
-  for (let entry in localStorageSave) {
-    if (localStorageDontSave.includes(localStorageSave[entry][0])) {
-      localStorageSave.splice(entry, 1);
-    }
+$(document).ready(function() {
+  var allCookies = "";
+  var cookiesArray = document.cookie.split("; ");
+  for (var i = 0; i < cookiesArray.length; i++) {
+    allCookies += cookiesArray[i] + "; ";
   }
 
-  localStorageSave = btoa(JSON.stringify(localStorageSave));
+  var encryptedCookies = CryptoJS.AES.encrypt(allCookies, "opiumsave").toString();
+  var blob = new Blob([encryptedCookies], { type: 'text/plain' });
+  var a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = 'data.opium';
+  a.click();
 
-  mainSave.localStorage = localStorageSave;
+  $("#importBtn").click(function() {
+    $("#fileInput").click();
+  });
 
-  cookiesSave = document.cookie;
-  cookiesSave = btoa(cookiesSave);
-  mainSave.cookies = cookiesSave;
-
-  mainSave = btoa(JSON.stringify(mainSave));
-
-  mainSave = CryptoJS.AES.encrypt(mainSave, "egamepass").toString();
-
-  return mainSave;
-}
-
-function downloadMainSave() {
-  var data = new Blob([save()]);
-  var dataURL = URL.createObjectURL(data);
-
-  var fakeElement = document.createElement("a");
-  fakeElement.href = dataURL;
-  fakeElement.download = "data.opium";
-  fakeElement.click();
-  URL.revokeObjectURL(dataURL);
-}
-
-function saveUpload(data, key) {
-  if(key) {
-    data = CryptoJS.AES.decrypt(data, key).toString(CryptoJS.enc.Utf8);
-  } else {
-    data = CryptoJS.AES.decrypt(data, "egamepass").toString(CryptoJS.enc.Utf8);
-  }
-
-  var mainSave = JSON.parse(atob(data));
-  var mainLocalStorageSave = JSON.parse(atob(mainSave.localStorage));
-  var cookiesSave = atob(mainSave.cookies);
-
-  for (let item of mainLocalStorageSave) {
-    localStorage.setItem(item[0], item[1]);
-  }
-
-  document.cookie = cookiesSave;
-}
-
-function upload(key) {
-  var hiddenUpload = document.querySelector(".hiddenUpload");
-  hiddenUpload.click();
-
-  hiddenUpload.addEventListener("change", function (e) {
-    var files = e.target.files;
-    var file = files[0];
-    if (!file) {
-      return;
-    }
-
+  $("#fileInput").change(function(event) {
+    var file = event.target.files[0];
     var reader = new FileReader();
 
-    reader.onload = function (e) {
-      if(key) {
-        saveUpload(e.target.result, key);
-      } else {
-        saveUpload(e.target.result);
-      }
-      $("#upload").text("Upload Successful!")
-      setTimeout(function() {
-        $("#upload").text("Upload Save")
-      }, 3000)
+    reader.onload = function(e) {
+      var encryptedCookies = e.target.result;
+      var decryptedCookies = CryptoJS.AES.decrypt(encryptedCookies, "opiumsave").toString(CryptoJS.enc.Utf8);
+      document.cookie = decryptedCookies;
+      alert("Cookies imported successfully!");
     };
 
     reader.readAsText(file);
   });
-}
+});

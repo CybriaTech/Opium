b244598a96a523c4fe112490d075be21119fd422
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-03 17:54:00 -0600

    Update 320x50.js

--- js/ads/320x50.js
+++ js/ads/320x50.js
@@ -1,19 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var atOptions = {
-        'key': 'f31689761dc02e086d4fc684e3e3abcc',
-        'format': 'iframe',
-        'height': 50,
-        'width': 320,
-        'params': {}
-    };
-
-    var scriptElement = document.createElement('script');
-
-    scriptElement.src = '//www.topcreativeformat.com/' + atOptions.key + '/invoke.js';
-
-    scriptElement.type = 'text/javascript';
-
-    scriptElement.async = true;
-
-    document.body.appendChild(scriptElement);
-});
+	atOptions = {
+		'key' : 'f31689761dc02e086d4fc684e3e3abcc',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f31689761dc02e086d4fc684e3e3abcc/invoke.js"></scr' + 'ipt>');

e659b78d6dffc4cb487529a4100d6d3ba5b20fcd
SC0TT <100329425+sc0ttferren@users.noreply.github.com>
2024-02-16 21:38:44 -0800

    Flappy Bird Re-Brand

--- games/flappybird/files/assets/64308041/1/game.js
+++ games/flappybird/files/assets/64308041/1/game.js
@@ -105,7 +105,7 @@ Game.prototype.initialize = function() {
         var left = (screen.width / 2) - (640 / 2);
         var top = (screen.height / 2) - (380 / 2);
 
-        var shareText = encodeURIComponent("I scored " + this.score + " in Flappy Bird! Beat that! http://flappybird.playcanvas.com/ Powered by @playcanvas #webgl #html5");
+        var shareText = encodeURIComponent("I scored " + this.score + " in Flappy Bird! Beat that! https://opium-games.com/games/flappybird/ Powered by CybriaTech #webgl #html5 #opium #cybriatech");
         var shareUrl = "https://twitter.com/intent/tweet?text=" + shareText;
 
         var popup = window.open(shareUrl, 'name', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + 640 + ', height=' + 380 +', top=' + top + ', left=' + left);
